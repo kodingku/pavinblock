32b92fadf067e46878716c94559508f8615001f1
kodingku <adirahman@kukode.com>
2018-06-10 14:43:15 +0700

    update kontent

--- application/views/content/home.php
+++ application/views/content/home.php
@@ -8,12 +8,222 @@
 </section>
 <section class="g-mt-50 g-mb-30">
 	<h1 class="text-center">SINAR BARU CONBLOCK BOGOR</h1>
-	<p class="text-center">
+	<p class="text-center g-font-size-20">
 		Sinar Baru Conblock adalah pabrik pembuatan Paving Block, Buis Beton, Genteng Beton, Grass Blok, Loster Beton, U-Dicth. Lokasi pabrik kami terletak di Cibinong, Kabupaten Bogor. Dengan harga murah dan negotiable sesuai dengan jumlah pemesanan dan jarak tempuhnya. Dan kami juga menyediakan jasa pemasangan.
 		Sinar Baru Conblock adalah pabrik yang bergerak dibidang Precast, kami menjamin kualitas mutu dari produk kami. Dan kami sangat siap untuk memenuhi kebutuhan precast pada project pemerintah maupun swasta. 
 
 	</p>
 </section>
+<section class="container g-mt-50">
+  <div class="row">
+    <div class="col-lg-6 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url();?>assets/img/core/profile.jpg" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/about" class="g-color-white btn btn-md u-btn-3d u-btn-primary g-mr-10 g-mb-15">Profil Perusahaan</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+    <div class="col-lg-6 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url();?>assets/img/core/contact.jpg" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/kontak" class="g-color-white btn btn-md u-btn-3d u-btn-primary g-mr-10 g-mb-15">Kontak Kami</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+   </div>
+</section>
+
+
+<section class="container g-mt-50 g-mb-60">
+    <div class=" u-heading-v2-4--bottom g-brd-blue g-mb-45 text-center">
+      <h2 class="h1 u-heading-v2__title g-line-height-1_3 g-font-size-30 g-mb-minus-10"><i class="fas fa-list-ul"></i> Beberapa Produk Kami</h2>
+    </div>
+ 
+  <div class="row">
+    <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/bata.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/about" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Bata</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+    <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/cacing.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/kontak" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Cacing</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+     <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/segienam.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/kontak" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Segi Enam</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+     <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/fullpave.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/kontak" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Full Pave</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+
+   </div>
+
+   <div class="row">
+    <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/buisbeton.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/about" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Buis Beton</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+    <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/buisbetonbelah.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/kontak" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Buis Beton Belah</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+     <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/topiuskup.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/kontak" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Topi Uskup</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+     <div class="col-lg-3 g-mb-30">
+      <!-- Article -->
+      <article class="u-block-hover">
+        <figure class="u-bg-overlay g-bg-black-opacity-0_3--after">
+          <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?php echo base_url()?>assets/img/content/trihex.png" alt="Image Description">
+        </figure>
+
+       
+
+        <header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+          
+          <h3 class="h2 g-font-weight-600 text-center">
+            <a href="<?php echo base_url() ?>Home/kontak" class="g-color-white btn btn-md u-btn-3d u-btn-bluegray g-mr-10 g-mb-15"><i class="fas fa-external-link-alt"></i> Trihex</a>
+          </h3>
+        </header>
+      </article>
+      <!-- End Article -->
+    </div>
+
+
+   </div>
+
+</section>
+
+<!--
 <section id="powered" class="text-center g-py-80">
         <div class="container-fluid">
           <div class="container g-width-780 g-mb-60">
@@ -57,13 +267,9 @@
               <img class="img-fluid u-shadow-v2 g-rounded-10" src="<?php echo base_url();?>assets/img/content/trihex.png" alt="Image Description">
             </div>
 
-            
-
-            
           </div>
-             
-
-             
+  
        	</div>
       
      </section>
+-->
