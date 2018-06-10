a9797380298dab5bd89ec2eb22c27fb16b53be72
kodingku <adirahman@kukode.com>
2018-06-10 16:04:04 +0700

    update produk

--- application/views/content/produk.php
+++ application/views/content/produk.php
@@ -3,41 +3,46 @@
 	<h2 class="text-center">Daftar Produk dan Harga Pavin Block</h2>
 
 	<div class="row">
-		<div class="col-md-2">
+		<div class="col-md-4">
 			<div class="u-shadow-v2 g-mb-30">
 			  <img class="img-fluid u-shadow-v2 g-rounded-10" src="<?php echo base_url();?>assets/img/content/bata.png" alt="Image Description">
-				<a href="#!" class="btn btn-md u-btn-outline-red g-mr-10 g-mb-15">List Harga Bata</a>
+				<a href="<?php echo base_url() ?>Home/displayBata" class="btn btn-md u-btn-outline-red g-mr-10 g-mb-15">List Harga Bata</a>
 			</div>
 		</div>
-		<div class="col-md-2">
+		<div class="col-md-4">
 			<div class="u-shadow-v2 g-mb-30">
 			  <img class="img-fluid u-shadow-v2 g-rounded-10" src="<?php echo base_url();?>assets/img/content/cacing.png" alt="Image Description">			
 			  <a href="#!" class="btn btn-md u-btn-outline-red g-mr-10 g-mb-15">List Harga Cacing</a>
 			</div>
 		
 		</div>
-		<div class="col-md-2">
+		<div class="col-md-4">
 			<div class="u-shadow-v2 g-mb-30">
 			  <img class="img-fluid u-shadow-v2 g-rounded-10" src="<?php echo base_url();?>assets/img/content/segienam.png" alt="Image Description">
 			<a href="#!" class="btn btn-md u-btn-outline-red g-mr-10 g-mb-15">List Harga Segi Enam</a>
 			</div>
 		
 		</div>
-		<div class="col-md-2">
+		
+	</div>
+
+	<section class="g-mt-50">
+		<div class="row">
+		<div class="col-md-4">
 			<div class="u-shadow-v2 g-mb-30">
 			  <img class="img-fluid u-shadow-v2 g-rounded-10" src="<?php echo base_url();?>assets/img/content/trihex.png" alt="Image Description">
 			
 			<a href="#!" class="btn btn-md u-btn-outline-red g-mr-10 g-mb-15">List Harga Trihex</a>
 			</div>
 		</div>
-		<div class="col-md-2">
+		<div class="col-md-4">
 			<div class="u-shadow-v2 g-mb-30">
 			  <img class="img-fluid u-shadow-v2 g-rounded-10" src="<?php echo base_url();?>assets/img/content/fullpave.png" alt="Image Description">
 			  <a href="#!" class="btn btn-md u-btn-outline-red g-mr-10 g-mb-15">List Harga Full Pave</a>
 			</div>
 		
 		</div>
-		<div class="col-md-2">
+		<div class="col-md-4">
 			<div class="u-shadow-v2 g-mb-30">
 			  <img class="img-fluid u-shadow-v2 g-rounded-10" src="<?php echo base_url();?>assets/img/content/topiuskup.png" alt="Image Description">
 			
@@ -45,9 +50,8 @@
 			</div>
 		
 		</div>
-	</div>
-
-	
+		</div>
+	</section>
 	<h2 class="text-center">Daftar Produk dan Harga Buis Beton</h2>
 	<div class="row">
 		<div class="col-md-2">
