45b9ef7b90111d233ac64315a08a4347e2459c28
adirahman <kodingku@github.com>
2018-06-11 15:25:22 +0700

    update model

--- application/models/Material_model.php
+++ application/models/Material_model.php
@@ -10,11 +10,31 @@
 
  		$query = $this->db->get('tm_pavin');
  		return $query->result();
+ 	}
+
+ 	public function get_bata_info($bata_id){
+
+ 		$this->db->where('id_pavin',$bata_id);
+ 		$query = $this->db->get('tm_pavin');
+ 		return $query->row();
  	}	
  	
  	public function create_bata($data){
 
  		$query = $this->db->insert('tm_pavin',$data);
  		return $query;
+ 	}
+
+ 	public function delete_bata($id){
+
+ 		$this->db->where('id_pavin',$id);
+ 		$this->db->delete('tm_pavin');
+ 	}
+
+ 	public function edit_bata($bata_id,$data){
+
+ 		$this->db->where('id_pavin',$bata_id);
+ 		$this->db->update('tm_pavin',$data);
+ 		return true;
  	}
  }
